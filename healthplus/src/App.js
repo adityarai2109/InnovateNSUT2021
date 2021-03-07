d6262b57c07a859d027bd0dc33e6ec1a3fb4e8f3
Aditya Rai <adityarai2109@gmail.com>
2021-03-08 00:12:00 +0530

    User page structure implemented

--- healthplus/src/App.js
+++ healthplus/src/App.js
@@ -8,7 +8,8 @@ import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import { Table } from "react-bootstrap";
 import { Container } from "react-bootstrap";
-import { Carousel, Accordion, Card } from "react-bootstrap";
+import { Carousel, Accordion, Card, ListGroup } from "react-bootstrap";
+import Iframe from "react-iframe";
 
 function App() {
   return (
@@ -42,48 +43,50 @@ function App() {
       </>
 
       <div className="famousHospitals">
-        <div className="head">Famous Hospitals Near You</div>
+        <div className="maps">
+          <h2 className="tableheadings">Hospitals Near You </h2>
+          <div className="map">
+            <iframe
+              src="https://maps.google.com/maps?width=700&height=440&hl=en&q=London%2C%20United%20Kingdom+(eatrs)&ie=UTF8&t=&z=10&iwloc=B&output=embed"
+              scrolling="no"
+              width={800}
+              height={600}
+              frameBorder={0}
+            />
+
+            <Card style={{ width: "20vw", height: "600px" }}>
+              <Card.Img
+                variant="top"
+                src="https://source.unsplash.com/random/600x500"
+              />
+              <Card.Body>
+                <Card.Title>Hospital's name</Card.Title>
+                <Card.Text>
+                  <ul className="hospitaldesc" style={{ textAlign: "left" }}>
+                    <li>
+                      Description : Some quick example text to build on the card
+                      title and make up the bulk of the card's content.
+                    </li>
+                    <li>Address : esgrtdh</li>
+                    <li>Email : fdzgxf@gxfhgcn.com</li>
+                    <li>Contact : 4325635</li>
+                  </ul>
+                </Card.Text>
+                <Button variant="success">Book an appointment</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
         <div className="FHcontainer">
-          {/* <div className="category"> Heart </div> */}
-          <Container>
-            <Table striped bordered hover className="text-dark">
-              <thead>
-                <tr>
-                  <th>Name </th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
-              </tbody>
-            </Table>
-          </Container>
           <div className="hospitaltable">
             <Carousel>
               <Carousel.Item interval={1000} className="carouselitem">
                 <Container>
-                  <Table striped bordered hover className="text-dark">
+                  <h4 className="tableheadings">Best hospitals for Kidney</h4>
+                  <Table striped bordered hover variant="dark">
                     <thead>
                       <tr>
-                        <th>Name </th>
+                        <th>#</th>
                         <th>First Name</th>
                         <th>Last Name</th>
                         <th>Username</th>
@@ -99,49 +102,207 @@ function App() {
                       <tr>
                         <td>2</td>
                         <td>Jacob</td>
-                        <td>Thornton</td>
+                        <td>Thosrhtjdyfkrnton</td>
                         <td>@fat</td>
                       </tr>
                       <tr>
                         <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
                       </tr>
                     </tbody>
                   </Table>
                 </Container>
               </Carousel.Item>
               <Carousel.Item interval={1000} className="carouselitem">
-                <img
-                  className="d-block w-60 h-40"
-                  src="https://source.unsplash.com/random/400x300"
-                  alt="Second slide"
-                />
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
+                <Container>
+                  <h4 className="tableheadings">Best hospitals for Kidney</h4>
+                  <Table striped bordered hover variant="dark">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thosrhtjdyfkrnton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Container>
               </Carousel.Item>
               <Carousel.Item interval={1000} className="carouselitem">
-                <img
-                  className="d-block w-60 h-40"
-                  src="https://source.unsplash.com/random/400x300"
-                  alt="Third slide"
-                />
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
+                <Container>
+                  <h4 className="tableheadings">Best hospitals for Kidney</h4>
+                  <Table striped bordered hover variant="dark">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thosrhtjdyfkrnton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </Container>
               </Carousel.Item>
             </Carousel>
           </div>
         </div>
         <div className="pricing"></div>
+      </div>
+      <div className="myappointments">
+        <Container>
+          <Card variant="dark" style={{ width: "100%" }}>
+            <Card.Header>My Appointments</Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Table striped bordered hover variant="dark">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Table striped bordered hover variant="dark">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Table striped bordered hover variant="dark">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Username</th>
+                    </tr>
+                  </thead>
+                </Table>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Container>
+      </div>
+      <div className="footer">
+        {/* Footer */}
+        <footer className="bg-dark text-center text-white">
+          {/* Grid container */}
+          <div className="container p-4">
+            {/* Section: Form */}
+            <section className>
+              <form action>
+                {/*Grid row*/}
+                <div className="row d-flex justify-content-center">
+                  {/*Grid column*/}
+                  <div className="col-auto">
+                    <p className="pt-2">
+                      <strong>Sign up for our newsletter</strong>
+                    </p>
+                  </div>
+                  {/*Grid column*/}
+                  {/*Grid column*/}
+                  <div className="col-md-5 col-12">
+                    {/* Email input */}
+                    <div className="form-outline form-white mb-4">
+                      <input
+                        type="email"
+                        id="form5Example2"
+                        className="form-control"
+                        placeholder="Email address..."
+                      />
+                    </div>
+                  </div>
+                  {/*Grid column*/}
+                  {/*Grid column*/}
+                  <div className="col-auto">
+                    {/* Submit button */}
+                    <button type="submit" className="btn btn-primary mb-4">
+                      Subscribe
+                    </button>
+                  </div>
+                  {/*Grid column*/}
+                </div>
+                {/*Grid row*/}
+              </form>
+            </section>
+            {/* Section: Form */}
+            {/* Section: Text */}
+            <section className="mb-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                distinctio earum repellat quaerat voluptatibus placeat nam,
+                commodi optio pariatur est quia magnam eum harum corrupti dicta,
+                aliquam sequi voluptate quas.
+              </p>
+            </section>
+            {/* Section: Text */}
+          </div>
+          {/* Grid container */}
+          {/* Copyright */}
+          <div
+            className="text-center p-3"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+          >
+            © 2020 Copyright:
+            <a className="text-white" href="https://mdbootstrap.com/">
+              Health+
+            </a>
+          </div>
+          {/* Copyright */}
+        </footer>
+        {/* Footer */}
       </div>
     </div>
   );
